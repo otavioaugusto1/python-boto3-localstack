88e1755cb847315336116c861915d89a33ad6f70
otavioaugusto1 <otavio.a.nogueira@hotmail.com>
2024-12-07 23:09:26 -0300

    Commit inicial do projeto

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import boto3
 from botocore.exceptions import ClientError
+from decouple import config
 
 def create_bucket(bucket_name):
     """Cria um bucket no LocalStack S3"""
     s3_client = boto3.client(
         's3', 
         endpoint_url='http://localhost:4566',  # URL padrão do LocalStack
-        aws_access_key_id='test',
-        aws_secret_access_key='test',
+        aws_access_key_id=config('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY'),
         region_name='us-east-1'
     )
     
